fcf65cac6b512a8f6ff2de5fb6ea97d74f7b07d9
nquocsuu <91043046+nquocsuu@users.noreply.github.com>
2022-05-20 11:11:15 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+// A URL returns TEXT data.
+import fetch from "node-fetch";
 
 // A URL returns TEXT data.
 var url = "https://ex1.o7planning.com/_testdatas_/simple-text-data.txt";
@@ -7,15 +9,13 @@ function doGetTEXT()  {
 
   // Call fetch(url) with default options.
   // It returns a Promise object (Resolve response object)
-  var aPromise = fetch(url, { 
-    mode: 'no-cors' // 'cors' by default
-});
+  var aPromise = fetch(url);
 
   // Work with Promise object:
   aPromise
     .then(function(response) {
         console.log("OK! Server returns a response object:");
-        console.log(response);
+        document.getElementsByTagName('p').innerHTML(response)
     })
     .catch(function(error)  {
         console.log("Noooooo! Something error:");
